af0c0eb39bb6c354482fae99e3d8868144edeb76
Jonatas-Correa-Dev <jonatasferreiracorrea@gmail.com>
2025-06-23 07:51:25 -0300

    new_fixes

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+
+# Cria os diretórios 'uploads' e 'audiobooks' se eles não existirem.
+# Isso garante que o aplicativo tenha onde salvar arquivos temporários.
+mkdir -p uploads
+mkdir -p audiobooks
+
+# Inicia o servidor Uvicorn para o seu aplicativo FastAPI
+# main:app significa que 'app' é a instância FastAPI no arquivo 'main.py'
 uvicorn main:app --host 0.0.0.0 --port $PORT
